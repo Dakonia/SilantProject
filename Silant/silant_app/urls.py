fc14aa1e5c13ddce0a80a61e71c23786dbfa14c5
Vlad <vladmen13@mail.ru>
2024-04-21 15:22:49 +0300

    add login, search

--- Silant/silant_app/urls.py
+++ Silant/silant_app/urls.py
@@ -1,8 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import MachineListView
+from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', MachineListView.as_view(), name='machine-list'),
-    # Добавьте другие маршруты, если это необходимо
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'), 
 ]
--- Silant/silant_app/views.py
+++ Silant/silant_app/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView
-from .models import Machine
+from .models import *
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
 
 class MachineListView(ListView):
     model = Machine
@@ -9,6 +12,38 @@ class MachineListView(ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        # Фильтрация по дате отгрузки с завода (для примера, фильтрация по дате больше 2020-01-01)
-        queryset = queryset.filter(shipment_date__gte='2020-01-01')
+        serial_number = self.request.GET.get('serial-number')
+        if serial_number:
+            queryset = queryset.filter(serial_number=serial_number)
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        serial_number = self.request.GET.get('serial-number')
+        if serial_number:
+            if context['machines']:
+                context['search_message'] = f''
+            else:
+                context['search_message'] = f'Данная машина с номером "{serial_number}" не найдена.'
+            context['search_header'] = 'Результат поиска:'
+        return context
+    
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('machine-list')  # Перенаправление на главную страницу
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
